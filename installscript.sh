ba00114d36b209280584882aa55975709e9305e2
Andrew Hobden <andrew@hoverbear.org>
2013-06-20 22:33:36 +0000

    Better install script

--- installscript.sh
+++ installscript.sh
@@ -22,13 +22,34 @@ sudo yum install \
   python-pip \
   python-ipython \
   python-psutil \
+  https://dl.google.com/linux/direct/google-chrome-unstable_current_x86_64.rpm \
   -y
 
 # Configure Git
 git config --global push.default simple
+git config --global credential.helper store
 
 # Clone in Powerline
 pip install --user git+git://github.com/Lokaltog/powerline
 
 # Dotfiles
-git clone https://github.com/Hoverbear/dotfiles.git dotfiles
+git clone https://github.com/Hoverbear/dotfiles.git ~/dotfiles
+mkdir -p ~/.config
+mv ~/.config/powerline ~/powerline_old
+ln -s ~/dotfiles/config/powerline ~/.config/powerline
+mv ~/.vimrc ~/vimrc_old
+ln -s ~/dotfiles/vimrc ~/.vimrc
+mv ~/.tmux.conf ~/tmux.conf_old
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+mv ~/.zshrc ~/zshrc_old
+ln -s ~/dotfiles/zshrc ~/.zshrc
+mv ~/.bashrc ~/bashrc_old
+ln -s ~/dotfiles/bashrc ~/.bashrc
+
+# Font
+mkdir -p ~/.fonts/DejaVu\ Sans\ Mono\ for\ Powerline
+cp ~/dotfiles/fonts/DejaVu\ Sans\ Mono\ for\ Powerline.ttf ~/.fonts/DejaVu\ Sans\ Mono\ for\ Powerline/DejaVu\ Sans\ Mono\ for\ Powerline.ttf 
+fc-cache -v
+
+# Change Shell
+chsh -s /bin/zsh
