2ac79a8fda570732a7e5ac4115ab2f96c6acfa7d
Andrew Hobden <andrew@hoverbear.org>
2013-07-22 16:55:27 -0700

    Added stylus.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # vim
-cp -i -R vim ~/.vim
-cp -i vimrc ~/.vimrc
+cp -R vim ~/.vim
+cp vimrc ~/.vimrc
 
 # zsh
-cp -i -R oh-my-zsh ~/.oh-my-zsh
-cp -i zshrc ~/.zshrc
+cp -R oh-my-zsh ~/.oh-my-zsh
+cp zshrc ~/.zshrc
